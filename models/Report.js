ae2e683f431b4822ae4fe78cdaebd6312895a2dc
campregher07 <danieljunioor38@gmail.com>
2025-06-03 19:52:37 -0300

    Fix reportSchema type for anonimo field from Int32Array to Int32

--- models/Report.js
+++ models/Report.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const reportSchema = new mongoose.Schema({
     anonimo: {
-        type: Int32Array,
+        type: Int32,
         default: false
     },
     nome: {
