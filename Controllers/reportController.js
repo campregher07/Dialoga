a788ef46b361db2892b5426acdd347d95da72852
campregher07 <danieljunioor38@gmail.com>
2025-06-05 19:54:35 -0300

    Enhance error handling in report submission with SweetAlert integration and improve validation feedback

--- Controllers/reportController.js
+++ Controllers/reportController.js
@@ -5,6 +5,15 @@ exports.Denunciar = async (req, res) => {
     const { anonimo, tipoDenuncia, ocorrido  } = req.body;
     const { username } = req.session;
 
+    if (!ocorrido || ocorrido.trim() === '') {
+        console.log("Preencha todos os campos");
+        return res.render("Reports/denuncias", { 
+            erroMessage: "Preencha todos os campos", 
+            username, 
+            currentPage: "report" 
+        });
+    }
+
     const newReport = {
         tipoDenuncia,
         ocorrido,
@@ -17,16 +26,18 @@ exports.Denunciar = async (req, res) => {
         newReport.anonimo = true;
     }
 
+
+
     try {
         await Report.create(newReport);
 
         console.log("Registro do diário salvo com sucesso no MongoDB Atlas!");
         res.redirect("/Denuncias"); 
+
     } catch (err) {
         console.error("Erro ao registrar denúncia no MongoDB:", err);
         res.status(500).render("Reposts/denuncias", { 
             username, 
-            userId, 
             currentPage: "report", 
             erroMessage: "Erro interno ao salvar denúncia." 
         });
